2f992071d17780739edf1b68eb7f75a3297f10c8
mohdanas07 <mohdanasgour7866@gmail.com>
2025-06-17 21:02:08 +0530

    Initial Commit

---
+++ SmartAid/SmartAidApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartAidApp.swift
+//  SmartAid
+//
+//  Created by mohdanas on 17/06/25.
+//
+
+import SwiftUI
+
+@main
+struct SmartAidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
